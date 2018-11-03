a8fb8bd99f72276c38989bba3c1aa8bf15017019
Burnsy <btburns10@gmail.com>
2018-11-02 23:20:56 -0400

    deploy bug fix

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static("public"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 app.use(routes);
-
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytimesdb";
 mongoose.connect(MONGODB_URI);
